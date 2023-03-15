500828633b7b4c47471ae732d4407bb04a52692e
Your Name <you@example.com>
2023-03-15 17:20:32 +0700

    asd

--- praktikum_4_mod4_1302210048/praktikum_4_mod4_1302210048/posisikaraktergame.cs
+++ praktikum_4_mod4_1302210048/praktikum_4_mod4_1302210048/posisikaraktergame.cs
@@ -8,5 +8,41 @@ namespace praktikum_4_mod4_1302210048
 {
     internal class posisikaraktergame
     {
+       public enum state
+        {
+            Jongkok,
+            Berdiri,
+            Terbang,
+            Tengkurap,
+        }
+        public enum Tombol
+        {
+            Tombol_W,
+            Tombol_S,
+            Tombol_X,
+        }
+        public class gerak
+        {
+            public state prevstate;
+            public state nextstate;
+            public Tombol tombol;
+
+            public  gerak(state prevstate, state nextstate, Tombol tombol)
+               {
+                 this .prevstate = prevstate;
+                 this .nextstate = nextstate;
+                 this .tombol = tombol;
+
+               }
+
+            
+        }
+        public static void Main()
+        {
+            gerak[] pergerakan =
+            {
+                new gerak(state)
+            }
+        }
     }
 }
